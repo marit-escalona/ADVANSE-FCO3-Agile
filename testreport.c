235a3229e39e2e4339d67771a7889e09f11c92d7
Lexi Valencia <valencialexi@gmail.com>
2013-03-15 20:15:19 -0700

    Test Report Template with header

---
+++ testreport.c
@@ -0,0 +1,52 @@
+#include "testreport.h"
+#include<stdio.h>
+#include<stdlib.h>
+
+TestReport *init_testReport(){
+	TestReport *testReport;
+	
+	if((testReport = malloc(sizeof(struct TestReport))) == NULL){
+		printf("Error: NOT ENOUGH MEMORY! \n");
+		exit(1);
+	}
+	
+	testReport->testReportNumber = 1; 
+	testReport->nextTestReport = NULL;
+	return testReport;
+}
+
+TestReport *add_testReport(TestReport *testReport, char testReportDesc[], char testReportConditions[], char expectedResults[], char actualResults[]){
+	TestReport *testReportCurrent;
+	TestReport *testReportNext;
+	int testReportNumber = 1;
+	
+	if(testReport == NULL){
+		testReportNext = store_to_NextTestReport(testReport, 1, testReportDesc, testReportConditions, expectedResults, actualResults);
+	}
+	
+	else{
+	
+		testReportCurrent = testReport;
+	
+		while(testReportCurrent->nextTestReport!=NULL){
+			testReportCurrent = testReportCurrent->nextTestReport;
+			testReportNumber++;
+		}
+	
+		testReportNext = store_to_NextTestReport(testReportNext, testReportNumber, testReportDesc, testReportConditions, expectedResults, actualResults);
+		testReportCurrent->nextTestReport = testReportNext;
+	}
+	
+	return testReport; 
+}
+
+TestReport *store_to_NextTestReport(TestReport *testReportNext, int testReportNumber, char testReportDesc[], char testReportConditions[], char expectedResults[], char actualResults[]){
+	
+	testReportNext->testReportNumber = testReportNumber;
+	testReportNext->testReportDesc = testReportDesc;
+	testReportNext->testReportConditions = testReportConditions;
+	testReportNext->expectedResults = expectedResults;
+	testReportNext->actualResults = actualResults;
+	
+	return testReportNext;
+}
---
+++ testreport.h
@@ -0,0 +1,13 @@
+
+#ifndef _TESTREPORT_H_
+
+struct TestReport{
+	int testReportNumber;
+	char testReportDesc[500];
+	char testReportConditions[500];
+	char expectedResults[500];
+	char actualResults[500];
+	struct TestReport *nextTestReport;
+};
+
+#endif
